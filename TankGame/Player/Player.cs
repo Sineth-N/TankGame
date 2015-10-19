e749b31004d10348642e0b86376e7df04ebb52d8
Sineth-N <sinethneranjana@gmail.com>
2015-10-19 23:52:40 +0530

    Now the Player Class is created and initial player data is stored

---
+++ TankGame/Player/Player.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TankGame.Player
+{
+    class Player
+    {
+        private int playerNumber;
+        private int playerLocationX;
+        private int playerLocationY;
+        private int direction;
+
+        public void setPlayerNumber(int number)
+        {
+            this.playerNumber = number;
+        }
+        public int getPlayerNumber()
+        {
+            return playerNumber;
+        }
+        public void setPlayerLocationX(int x)
+        {
+            this.playerLocationX = x;
+        }
+        public int getPlayerLocationX()
+        {
+            return playerLocationX;
+        }
+        public void setPlayerLocationY(int y)
+        {
+            this.playerLocationY = y;
+        }
+        public int getPlayerLocationY()
+        {
+            return playerLocationY;
+        }
+
+        public void setDirection(int p)
+        {
+            this.direction = p;
+        }
+        public void ToString()
+        {
+            Console.WriteLine("Player Number:" + this.playerNumber + "\n X Coordinate" + this.playerLocationX
+                + "\n Y Coordinate" + this.playerLocationY + "\n Current Direction " + this.direction
+                );
+        }
+
+    }
+}
--- TankGameUnitTest/UnitTest2.cs
+++ TankGameUnitTest/UnitTest2.cs
@@ -12,15 +12,24 @@ namespace TankGameUnitTest
         private const String GAME_ALREADY_STARTED = "GAME_ALREADY_STARTED";
         
         Main main = new Main();
+
         [TestMethod]
         public void LoginReqTest()
         {
             Assert.AreEqual("JOIN#", main.joinserver());
         }
+
         [TestMethod]
         public void LoginReplyTest()
         {
             Assert.AreEqual(3, main.serverJoinReply(GAME_ALREADY_STARTED));
         }
+
+        [TestMethod]
+        public void AcceptanceTest()
+        {
+            Assert.AreEqual(1, main.acceptance("s:p1:1,1:0#"));
+        }
+
     }
 }
