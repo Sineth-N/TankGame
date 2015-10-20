5af9a5077a58fb21f17b2f5076db2c8182bab1ba
Sineth-N <sinethneranjana@gmail.com>
2015-10-20 20:19:00 +0530

    Player Class is Set

---
+++ TankGame/Player/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TankGame.Player
+{
+    class Player
+    {
+        public int playerNumber { get; set; }
+        public int playerLocationX { get; set; }
+        public int playerLocationY { get; set; }
+        public int direction { get; set; }
+    }
+}
