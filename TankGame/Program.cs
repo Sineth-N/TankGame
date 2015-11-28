e53b7d4385216fe9e6cc7dde0da9f04e6aafea39
Heshan-Sandamal <mhsandamal1@gmail.com>
2015-11-29 00:34:31 +0530

    connecting to server

--- TankGame/Program.cs
+++ TankGame/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TankGame.Client;
+
 
 namespace TankGame
 {
@@ -10,7 +12,8 @@ namespace TankGame
     {
         static void Main(string[] args)
         {
-            
+            //init client connection to server
+            ClientConnectionInit.Connect();
         }
        
     }
